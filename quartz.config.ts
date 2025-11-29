4d24d3c2dea434cce9976bb0b083e8dea7fb1a7f
adityashibu <as2397@hw.ac.uk>
2025-11-29 19:10:33 +0400

    Header

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "AtlasRacing Wiki",
+    pageTitle: "Atlas Wiki",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
